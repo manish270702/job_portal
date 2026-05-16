60c51c010ea9789076b5e2866093960ac4b4c999
manish270702 <my602382@gmail.com.com>
2026-05-16 18:22:41 +0530

    trying to resolve cookie problem in frontend and it fail

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'http://localhost:3000/api', // Replace with your API URL
   timeout: 10000,
+  withCredentials:true,
   headers: {
     'Content-Type': 'application/json'
   }
