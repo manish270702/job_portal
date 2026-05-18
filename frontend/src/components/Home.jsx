3f95f32e2cbf58e699e9021b96d43f82d48c600a
manish270702 <my602382@gmail.com.com>
2026-05-18 07:46:11 +0530

    react ifinite scroller applied

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,47 +2,58 @@ import React from 'react'
 import axios from '../api/api'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function Home() {
 
   const [data, setdata] = useState([])
-  const [page, setpage] = useState(0)
+  const [page, setpage] = useState(1)
+  const [totalpages, setTotalpages] = useState(0)
 
   const fetchData = async () => {
-    setpage(prev=>prev+1)
-    const response = await axios.get('/jobs',{
+    setpage(prev => prev + 1)
+    const response = await axios.get('/jobs', {
       params: {
         page,
-        limit:20
+        limit: 20
       }
     })
-    console.log(response.data.data)
     setdata([...data, ...response.data.data])
+
+    setTotalpages(response.data.pagination.pages)
   }
+  const checkhasmore = () => {
+    if (totalpages < page) {
+      return false
+    }
+    return true
+    console.log(totalpages);
+  }
+
 
   useEffect(() => {
     fetchData()
   }, [])
 
-  useEffect(() => {
-    // fetchData()
-  }, [page])
-
   return (
-    <>
-      <div className='grid grid-cols-5 gap-5 px-8'>
-        {data.map((item) => {
-          return <div key={item._id} className="border rounded-md p-3">
-            <h1 className="text-xl font-bold">{item.title}</h1>
-            <p>{item.company}</p>
-            <p>{item.location}</p>
-            <p>{item.salary}</p>
-            <p>{item.jobtype}</p>
-          </div>
-        })}
-      </div>
-      <button onClick={fetchData} className="bg-blue-500 mt-5 text-white px-4 py-2 rounded-md">Fetch Jobs</button>
-    </>
+        <InfiniteScroll
+          className='grid grid-cols-5 gap-5 px-8'
+          dataLength={data.length}
+          next={fetchData}
+          hasMore={checkhasmore()}
+          loader={checkhasmore() ? <h4>Loading...</h4>:null}
+          scrollableTarget="scrollableDiv"
+        >
+          {data.map((item) => {
+            return <div key={item._id} className="border rounded-md p-3">
+              <h1 className="text-xl font-bold">{item.title}</h1>
+              <p>{item.company}</p>
+              <p>{item.location}</p>
+              <p>{item.salary}</p>
+              <p>{item.jobtype}</p>
+            </div>
+          })}
+        </InfiniteScroll>
   )
 }
 
